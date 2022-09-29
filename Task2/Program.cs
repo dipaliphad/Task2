ca932bc899bf40171921246204a086a4594ccb73
rupesh palve <rupesh.palve48@outlook.com>
2022-09-29 14:46:10 -0400

    covert into List

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,13 +6,12 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    public class Program
-    {
+    
         public class Department
         {
             public int Id { get; set; }
             public string Name { get; set; }
-            public Employee[] employees { get; set; }
+            public List<Employee> employees { get; set; }
 
         }
         public class Employee
@@ -22,42 +21,49 @@ namespace Task2
             public double Salary { get; set; }
             public string Gender { get; set; }
         }
-            
+    public class program1
+    {
         static void Main(string[] args)
         {
-            Employee[] emp1 = new Employee[]
+            List<int> data = new List<int>();
+
+
+            List<Employee> emp1 = new List<Employee>();
             {
-                new Employee{Name="Rahul",Age=25,Salary=40000,Gender="male"},
-                new Employee{Name="Kajal", Age=26, Salary=45000,Gender="female"},
+                emp1.Add(new Employee { Name = "Rahul", Age = 25, Salary = 40000, Gender = "male" });
+                emp1.Add(new Employee { Name = "Kajal", Age = 26, Salary = 45000, Gender = "female" });
             };
 
-            Employee[] emp2 = new Employee[]
+            List<Employee> emp2 = new List<Employee>();
             {
-             new Employee{Name="sam",Age=26,Salary=50000,Gender="male"},
-             new Employee{Name="Komal", Age=24, Salary=41000,Gender="female"},
-             new Employee{Name="Arati", Age=23, Salary=35000,Gender="female"},
+                emp2.Add(new Employee { Name = "sam", Age = 26, Salary = 50000, Gender = "male" });
+                emp2.Add(new Employee { Name = "Komal", Age = 24, Salary = 41000, Gender = "female" });
+                emp2.Add(new Employee { Name = "Arati", Age = 23, Salary = 35000, Gender = "female" });
             };
-            Employee[] emp3 = new Employee[]
+            List<Employee> emp3 = new List<Employee>
             {
              new Employee{Name="Karan",Age=27,Salary=70000,Gender="male"},
             new Employee{Name="Kriya", Age=24, Salary=48000,Gender="female"},
             };
 
-            Department[] departments = new Department[]
+            List<Department> departments = new List<Department>
             {
              new Department{Id=1,Name="design",employees=emp1},
              new Department{Id=2,Name="quality",employees=emp2},
              new Department{Id=3,Name="purches",employees=emp3},
             };
 
-            foreach(Department d in departments)
+            foreach (Department d in departments)
             {
                 Console.WriteLine($"{d.Id} {d.Name}");
-                foreach(Employee e in d.employees)
+                foreach (Employee e in d.employees)
                 {
                     Console.WriteLine($"\t{e.Name}->{e.Age}->{e.Salary}->{e.Gender}");
                 }
             }
         }
-    }
+
+
+    }    
+    
 }
